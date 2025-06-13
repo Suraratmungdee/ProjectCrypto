654c59190f39fc06290c6dc590b9851e3dd25284
Suraratmungdee <45279243+Suraratmungdee@users.noreply.github.com>
2025-06-13 13:37:28 +0700

    แก้ไข mig

--- migrations/20250611142454_create_wallets_table.ts
+++ migrations/20250611142454_create_wallets_table.ts
@@ -4,7 +4,7 @@ export async function up(knex: Knex): Promise<void> {
     return knex.schema.createTable("wallets", (table) => {
         table.increments("wallet_id").primary();
         table.integer("user_id").unsigned().references("user.user_id").onDelete("CASCADE");
-        table.enu("currency_code", ["BTC", "ETH", "XRP", "DOGE"]).notNullable();
+        table.enu("currency_code", ["BTC", "ETH", "XRP", "DOGE", "THB", "USD"]).notNullable();
         table.decimal("balance", 30, 10).defaultTo(0);
         table.timestamp("created_at").defaultTo(knex.fn.now());
         table.timestamp("updated_at").defaultTo(knex.fn.now());
